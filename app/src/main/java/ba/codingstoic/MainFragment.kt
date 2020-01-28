11f4425c731bc4fbb407a4272e88fb163a3b37af
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2020-01-28 07:58:27 +0100

    instantiate view model in MainFragment, add getPodcasts method to the view model, show / hide content if loading

--- app/src/main/java/ba/codingstoic/MainFragment.kt
+++ app/src/main/java/ba/codingstoic/MainFragment.kt
@@ -2,28 +2,44 @@ package ba.codingstoic
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_main.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+    lateinit var viewModel: PodcastsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity()).get(PodcastsViewModel::class.java)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // todo implement navigation to details view
+
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer {
+            loading_indicator.visibility = if (it) View.VISIBLE else View.GONE
+            content_group.visibility = if (it) View.GONE else View.VISIBLE
+        })
+        viewModel.podcasts.observe(viewLifecycleOwner, Observer {
+            podcast_title.text = it.first().name
+        })
+
+        viewModel.getPodcasts()
     }
-
-
 }
--- app/src/main/java/ba/codingstoic/PodcastsViewModel.kt
+++ app/src/main/java/ba/codingstoic/PodcastsViewModel.kt
@@ -1,23 +1,27 @@
 package ba.codingstoic
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 data class Podcast(val name: String)
 
-class PodcastsViewModel: ViewModel() {
-    val _podcasts = MutableLiveData<List<Podcast>>()
+class PodcastsViewModel : ViewModel() {
+    private val _podcasts = MutableLiveData<List<Podcast>>()
     val podcasts: LiveData<List<Podcast>> = _podcasts
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
 
-    fun getPodcasts(){
+    fun getPodcasts() {
+        _isLoading.value = true
         viewModelScope.launch {
             delay(TimeUnit.SECONDS.toMillis(2))
             _podcasts.value = listOf(Podcast("The Joe Rogan experience"))
+            _isLoading.value = false
         }
     }
 }
