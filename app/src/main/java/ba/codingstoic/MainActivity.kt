9c2bedf2952adbd5edda52f9205d21e678777713
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2020-05-21 02:00:44 +0200

    add app wide mini player view

--- app/src/main/java/ba/codingstoic/MainActivity.kt
+++ app/src/main/java/ba/codingstoic/MainActivity.kt
@@ -1,23 +1,27 @@
 package ba.codingstoic
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import ba.codingstoic.player.PlayerViewModel
-import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.exoplayer2.ExoPlayer
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
-    private val playerViewModel: PlayerViewModel by inject()
+    private val playerViewModel: PlayerViewModel by viewModel()
+    private val exoPlayer: ExoPlayer by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        player_view.player = exoPlayer
+
         playerViewModel.currentlyPlaying.observe(this, Observer {
-            BottomSheetBehavior.from(main_activity_bottom_sheet).state =
-                BottomSheetBehavior.STATE_EXPANDED
+            main_activity_bottom_sheet.visibility = View.VISIBLE
         })
     }
 }
